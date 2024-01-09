d44ef69d5f583c6f3eda5a9e3534893f5efb4bfb
NachiketaTiwari69 <nachiketatiwari56@gmail.com>
2024-01-09 19:04:23 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,55 @@
+const upperCaseSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCaseSet = "abcdefghijklmnopqrstuvwxyz";
+const numberSet = "0123456789";
+const symbolSet = "!@#$%^&*()<>?:/"
+
+const passBox = document.getElementById("pass-box")
+const totalChar = document.getElementById("totalchar")
+const upperInput = document.getElementById("UpperCasechar")
+const lowerInput = document.getElementById("LowerCasechar")
+const numberInput = document.getElementById("Numberchar")
+const symbolInput = document.getElementById("Symbolchar")
+
+const getrandomdata = (dataset) => {
+    return dataset[Math.floor(Math.random() * dataset.length)];
+}
+
+
+const getrandompassword = (password = "") => {
+    if (upperInput.checked) {
+        password += getrandomdata(upperCaseSet);
+    }
+    if (lowerInput.checked) {
+        password += getrandomdata(lowerCaseSet);
+    }
+    if (numberInput.checked) {
+        password += getrandomdata(numberSet);
+    }
+    if (symbolInput.checked) {
+        password += getrandomdata(symbolSet);
+    }
+    if (password.length < totalChar.value) {
+        return getrandompassword(password);
+    }
+
+    passBox.innerText = truncateString(password, totalChar.value);
+    passBox.style.color = "#181818";
+    passBox.style.fontSize = "20px";
+}
+
+document.getElementById("btn").addEventListener("click",
+    function () {
+        getrandompassword();
+    }
+)
+
+
+function truncateString(str, num) {
+    if (str.length > num) {
+        let subStr = str.substring(0, num);
+        return subStr;
+    } else {
+        return str;
+    }
+}
+
